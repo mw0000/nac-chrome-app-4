89d1c4f936b2df2003fde689b6ee8c2e92aa04db
mw0000 <m@wilkowski.org>
2019-09-20 20:20:25 +0200

    random photos with titles

--- src/override/override.js
+++ src/override/override.js
@@ -3,7 +3,24 @@
 $( document ).ready(function() {
     $('h1').text('sdfsdf');
     console.log('test');
-    $.backstretch("https://audiovis.nac.gov.pl/i/PIC/PIC_1-G-1055.jpg");
+    
+    $.getJSON( "http://dormitorium.kei.pl/nac-api/api.php", function( data ) {
+        console.log(data);
+        var items = [];
+        $.each( data, function( key, val ) {
+          //items.push( "<li id='" + key + "'>" + val + "</li>" );
+        });
+       //
+       $.backstretch(data['img_url']);
+       $('p a').text(data['title']); 
+
+      });
+      
+
+
+
+    //var photo = $.JSON.parse('http://dormitorium.kei.pl/nac-api/api.php');
+    
 });
 
 
